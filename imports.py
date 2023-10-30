c7c8d2ad207ccde71ceb4d49afd243b24fbe436c
Ryan Millett <96157608+kr4g@users.noreply.github.com>
2023-10-30 10:11:40 -0700

    Update imports.py

--- imports.py
+++ imports.py
@@ -4,7 +4,13 @@ import pandas as pd
 import numpy as np
 import regex as re
 from fractions import Fraction
+from math import prod
+
+# because we love functional programming...
+from functools import reduce
 import itertools
+from itertools import chain
+
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import ipywidgets as widgets
