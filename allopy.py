c9e72aaad4a42be7c745d5574560db17a44c876f
Ryan Millett <96157608+kr4g@users.noreply.github.com>
2023-10-30 10:11:13 -0700

    Update allopy.py

--- allopy.py
+++ allopy.py
@@ -1,22 +1,3 @@
-import os
-import shutil
-import pandas as pd
-import numpy as np
-import regex as re
-from fractions import Fraction
-from math import prod
-
-# because we love functional programming...
-from functools import reduce
-import itertools
-from itertools import chain
-
-import matplotlib.pyplot as plt
-from matplotlib import cm
-import ipywidgets as widgets
-from IPython.display import display, clear_output, HTML
-
-
 def synthSeq_to_df(filepath):
   '''
   Parses a .synthSequence file and returns the score as a Pandas DataFrame
