cdd337108d39cf60e60b7049582ed6a070584d66
Ryan Millett <96157608+kr4g@users.noreply.github.com>
2023-12-07 11:43:55 -0800

    Delete allopy/skora/build directory

--- allopy/skora/build/skora/Release/CMakeFiles/skora_run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/skora_run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/skora_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
