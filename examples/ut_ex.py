5dd96262555362951266b1fb98af870aa1812963
Ryan Millett <ryanmillett@169-231-120-3.wireless.ucsb.edu>
2024-03-29 14:03:13 -0700

    revised example

--- examples/ut_ex.py
+++ examples/ut_ex.py
@@ -34,7 +34,7 @@ def pre_compositional_materials(G:tuple = (3, 5, 7, 11, 13)) -> dict:
         UT(tempus   = meas,
            prolatio = RT.from_tuple(group[i]),
            tempo    = 48,
-           beat     = '1/8') for i in range(len(group))
+           beat     = '1/16') for i in range(len(group))
     ]
     materials['ut_seq'] = ut_seq
     hx = tonos.scales.CPS(S, 2)
@@ -45,18 +45,16 @@ def layer1(material:dict, start_offset:float=0.05) -> list:
     note_list = []
     dyn = aikous.DYNAMICS
     attk_max = lambda d: d * 0.1667
-    amp_scale = lambda freq: aikous.amp_freq_scale(freq) * 0.75
+    amp_scale = lambda freq: aikous.amp_freq_scale(freq) * 0.45
     ratio_seq = material['hexany'].ratios
     np.random.shuffle(ratio_seq)
     ratio_seq = cycle(ratio_seq)
     for i, ut in enumerate(material['ut_seq']):
-        root_freq = tonos.pitchclass_to_freq('F#3') * material['hexany'].ratios[i]
+        # if i != 2: continue
+        root_freq = tonos.pitchclass_to_freq('F#2') * material['hexany'].ratios[i]
         pan = np.interp(i, [0, len(material['ut_seq'])], [-1, 1])
         # amFunc = np.random.choice([0,1,3])
-        amRatio = tonos.fold_interval(interval = float(np.random.choice(material['hexany'].products)),
-                                      equave   = 2,
-                                      equaves  = 2) * np.random.choice([1, 0.5])
-        
+        amRatio = 1.0
         for j, (onset, dur) in enumerate(ut):  # for each note event in the UT...
             if j == 0: continue # rest for the first note event
 
@@ -64,12 +62,12 @@ def layer1(material:dict, start_offset:float=0.05) -> list:
             new_row = skora.get_pfields(INST_FILEPATH, 'OscAM')
 
             # 2) perform any compositional logic
-            duration    = np.random.uniform(dur * 0.05, dur * 0.6)
+            duration    = np.random.uniform(dur * 0.0667, dur * 0.833)
             frequency   = root_freq * next(ratio_seq)
             attackTime  = np.random.uniform(0.0, attk_max(duration))
             releaseTime = np.interp(attackTime,                # map this value
                                     [0.0, attk_max(duration)], # from this range  
-                                    [dur, 0.0])                # to this range
+                                    [dur - duration, 0.0])     # to this range
             sustain     = np.random.uniform(0.5, 0.95)
             amplitude   = np.interp(attackTime, 
                                     [0.0, attk_max(duration)],
@@ -77,7 +75,11 @@ def layer1(material:dict, start_offset:float=0.05) -> list:
             am1         = np.random.randint(1)
             am2         = 0.0 if am1 == 1 else 1
             amRise      = duration * 0.25
-            amRatio     = amRatio
+            # amRatio     = amRatio
+            if j % len(material['hexany'].ratios) == 0:
+                amRatio = tonos.fold_interval(interval = float(np.random.choice(material['hexany'].products)),
+                                              equave   = 2,
+                                              equaves  = 2) * np.random.choice([2, 1, 0.5])
             reverb      = np.interp(attackTime, [0.0, attk_max(duration)], [0.07, 0.25])
 
             # 3) set the pfields for the note event
