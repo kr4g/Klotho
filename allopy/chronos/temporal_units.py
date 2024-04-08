bc2ab7156c912dea28d8dd4143436bdc652b59ab
Ryan Millett <ryanmillett@169-231-120-3.wireless.ucsb.edu>
2024-04-07 22:50:43 -0700

    ut beat and duration setters

--- allopy/chronos/temporal_units.py
+++ allopy/chronos/temporal_units.py
@@ -17,6 +17,9 @@ class UT:
         self.__prolationis = self._set_prolationis(prolatio) # RT object
         self.__tempo       = tempo
         self.__beat        = self._set_beat(beat, prolatio)
+
+        self.__onsets      = None
+        self.__durations   = None
     
     @classmethod
     def from_tree(cls, tree:Union[RT, tuple]):
@@ -57,15 +60,21 @@ class UT:
     
     @property
     def onsets(self):
-        return calc_onsets(self.durations)
+        if self.__tempo is None:
+            return None
+        if self.__onsets is None:
+            self.__onsets = calc_onsets(self.durations)
+        return self.__onsets
 
     @property
     def durations(self):
         if self.__tempo is None:
             return None
-        return tuple(beat_duration(ratio      = r,
-                                   bpm        = self.__tempo,
-                                   beat_ratio = self.__beat) for r in self.__prolationis.ratios)
+        if self.__durations is None:
+            self.__durations = tuple(beat_duration(ratio      = r,
+                                                   bpm        = self.__tempo,
+                                                   beat_ratio = self.__beat) for r in self.__prolationis.ratios)
+        return self.__durations
 
     @property
     def duration(self):
@@ -76,10 +85,14 @@ class UT:
     @tempo.setter
     def tempo(self, tempo:Union[None,float,int]):
         self.__tempo = tempo
+        self.__onsets = None
+        self.__durations = None
     
     @beat.setter
     def beat(self, beat:Union[str,Fraction]):
         self.__beat = Fraction(beat)
+        self.__onsets = None
+        self.__durations = None
         
     def decompose(self, prolatio:Union[RT,tuple,str] = 'd') -> 'UTSeq':
         if prolatio.lower() in {'s'}: prolatio = self.__prolationis.subdivisions
