f804848bc1cce7930336ee1d7adfe1eaad5dfbc0
Ryan Millett <ryanmillett@169-231-120-3.wireless.ucsb.edu>
2024-05-07 08:59:41 -0700

    sc examples

--- examples/sc_osc.py
+++ examples/sc_osc.py
@@ -13,48 +13,42 @@ from allopy.chronos import rhythm_trees as r_trees
 from allopy.chronos.rhythm_trees import rt_algorithms as rt_alg
 from allopy.chronos import temporal_units as ut
 from allopy.tonos.JI import combination_sets as cps
-from allopy.tonos import fold_freq
+from allopy.tonos import fold_freq, fold_interval
 from allopy.topos.sequences import Norg
+from allopy.aikous import amp_freq_scale
 
 client = udp_client.SimpleUDPClient('127.0.0.1', 57120)
 
-rts = [
-    r_trees.RT(time_signature='4/4', subdivisions=(3,(1,(2,1)),2,1,(1,(1,1,1)))),
-    r_trees.RT(time_signature='15/16', subdivisions=(3,2,1,3,1,2,3)),
-    r_trees.RT(time_signature='4/3', subdivisions=((4,(3,(8,(3,4)))),-3)),
-    r_trees.RT(time_signature='7/5', subdivisions=((4,(1,1,1)),(3,(1,)*8),-5)),
-    r_trees.RT(time_signature='21/12', subdivisions=(11,7,5,3)),
-]
+bpm = 36
+duration = 36
+tempus = '13/1'
+beat = '1/16'
 
-# u_t = ut.UT(tempus='7/5', prolatio=((4,(1,1,1)),(3,(1,)*8),-5), tempo=60, beat='1/5')
-i = -1
-u_t = ut.UT.from_tree(rts[i], tempo=60, beat='1/4')
-u_t.tempo = 36
-u_t.beat = f'1/{u_t.tempus.denominator}'
-hx = cps.Hexany()
-# freqs = cycle([fold_freq(1.0 * 333.0 * np.random.choice([r, 1/r])) for r in hx.ratios])
-# amps = cycle([np.random.uniform(0.05, 0.25) for _ in range(5)])
+p_mat = rt_alg.autoref_rotmat((5,17,7,13,3,11), 'G')
+rts = [r_trees.RT(duration=duration, time_signature=tempus, subdivisions=row) for row in p_mat]
 
-print(u_t.tree)
-events = [
-    # ('syn', 0.2, 'dur', 1, 'freq', 500, 'amp', 0.7),
-    # ('saw', 0.5, 'dur', 1.5, 'freq', 440, 'amp', 0.6),
-    # ('bassDrum', 0.0, 'dur', 0.4)
-]
-
-rt = r_trees.RT(duration=11, time_signature='9/4', subdivisions=rt_alg.autoref((7,5,13,3,11)))
-rt_mel = r_trees.RT(duration=11, time_signature='9/4', subdivisions=rt_alg.autoref(rt_alg.rhythm_pair((1,3,5))))
-
-for i, n in enumerate(hx.factors):
-    freqs = cycle([fold_freq(n * 166.5 * np.random.choice([r, 1/r]), lower=83.25) for r in hx.ratios])
-    amps = cycle([np.random.uniform(0.005, 0.05) for _ in range(np.random.randint(3, 7))])
-    for start, duration in ut.UT.from_tree(rt.rotate(i), tempo=36, beat='1/16'):
-    # for start, duration in u_t:
-        if duration < 0: continue
-        # print(start, duration)
-        freq = next(freqs)
-        amp = next(amps)
-        events.append(('syn', start, 'dur', duration*3.33, 'freq', freq, 'amp', amp))
+hx = cps.Pentadekany()
+factors = cycle(hx.factors)
+events = []
+synths = cycle(['vibraphone', 'celeste', 'glockenspiel', 'syn'])
+for i, rt in enumerate(rts):
+    amps = cycle([np.random.uniform(0.005, 0.07) for _ in range(np.random.randint(3, 7))])
+    for j, (start, duration) in enumerate(ut.UT.from_tree(rt, tempo=bpm, beat=beat)):
+        # if duration < 0: continue
+        if j == 0:
+            # if i == 0: 
+            #     events.append(('bassDrum', start, 'amp', 0.05))
+            continue
+        # idx = Norg.inf_num(j - 1) % len(hx.ratios)
+        # ratio = fold_interval(hx.ratios[idx] if idx >= 0 else 1/hx.ratios[abs(idx)], equave=2.0, n_equaves=1)
+        partial = Norg.inf_num((j - 1) * 3 + 7)
+        partial += 1 if partial >= 0 else -1
+        ratio = fold_interval(partial if partial > 0 else 1/abs(partial), equave=2.0, n_equaves=1)
+        f = next(factors)
+        freq = fold_freq(f * 166.5 * ratio, lower=83.25, upper=5328)
+        amp = next(amps) * amp_freq_scale(freq) * np.interp(freq, [83.25, 5328], [1.0, 0.35])
+        synth = next(synths)
+        events.append((synth, start, 'dur', duration*1.33, 'freq', freq, 'amp', amp))
 
 # m_freq = cycle([fold_freq(666.0 * hx.ratios[Norg.inf_num(i) % len(hx.ratios)], lower=166.5, upper=999.0) for i in range(11)])
 # for i, (start, duration) in enumerate(ut.UT.from_tree(rt_mel, tempo=36, beat='1/16')):
